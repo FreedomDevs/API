e1d7c17973b1129736f6e21b0725f5a15cdd0cef
foksik <107531680+Foksif@users.noreply.github.com>
2025-04-19 20:51:33 +0700

    баг фикс

--- main-api/src/server-request/server-request.controller.ts
+++ main-api/src/server-request/server-request.controller.ts
@@ -1,14 +1,16 @@
-import {Body, Controller, Get, NotFoundException, Param, Post, UseGuards} from '@nestjs/common'
+import { Body, Controller, Get, NotFoundException, Param, Post, UseGuards } from '@nestjs/common'
 import { ServerAccessGuard } from '@auth/guards/server-access.guard'
 import { UserService } from '@user/user.service'
 import { CheckTokenDto } from './dto'
+import { Public } from '@common/decorators'
 
+@Public()
 @UseGuards(ServerAccessGuard)
 @Controller('server-request')
 export class ServerRequestController {
   constructor(private readonly userService: UserService) {}
 
-  @Post('get/user/:username')
+  @Get('get/user/:username')
   getUser(@Param('username') username: string) {
     return this.userService.findOne(username)
   }
